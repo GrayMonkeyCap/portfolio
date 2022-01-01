46ed0fb3d4cb7b3b5e6cad943f61b697f5145d67
riyashukla6102 <71920274+riyashukla6102@users.noreply.github.com>
2022-01-02 01:54:40 +0530

    Added About and AOI Components
    Signed-off-by: riyashukla6102 <71920274+riyashukla6102@users.noreply.github.com>

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import styles from './Home.module.css'
+import AboutMe from '../components/AboutMe';
+import Aoi from '../components/Aoi';
+import Intro from '../components/Intro';
 
-const Home=()=>{
+const Home=(props)=>{
+    console.log(props)
     return(
         <>
-        <div className={styles.ellipse}></div>
-        <div className={styles.introContainer}>
-            <div className= {styles.intro}>
-                <div className={styles.text1}>Hi, I am</div>
-                <div className={styles.gradientText}>Harrison Jansma</div>
-                <div className={styles.text2}>Contemplative coder and analyst . Inspired by tough problems</div>
-                <button className={styles.projectbtn}>My Projects</button>
-            </div>
-
-            <div className={styles.profilepic}>
-            <img src='/profilepic.png'></img>
-            </div>
-        </div>
+        <Intro></Intro>
+        <AboutMe></AboutMe>
+        <Aoi></Aoi>
         </>
     );
 }
---
+++ src/components/Intro.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './Intro.module.css'
+
+const Intro=()=>{
+    return(
+        <>
+            
+            <div className={styles.introContainer}>
+                <div className= {styles.intro}>
+                <div className={styles.ellipse}></div>
+                    <div className={styles.text1}>Hi, I am</div>
+                    <div className={styles.gradientText}>Harrison Jansma</div>
+                    <div className={styles.text2}>Contemplative coder and analyst . Inspired by tough problems</div>
+                    <button className={styles.projectbtn}>My Projects</button>
+                </div>
+
+                <div className={styles.profilepic}>
+                <img src='/assets/profilepic.png'></img>
+                </div>
+            </div>
+            <div className={styles.arrows}>
+                <img src='/assets/arrows.png'></img>
+            </div>
+        </>
+        
+    )
+}
+
+export default Intro;
