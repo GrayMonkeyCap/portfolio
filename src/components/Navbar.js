17695b4c4e0b1c7537362f9ca0d3ef41c55da538
Graymonkeycap <akhilchakkungal@gmail.com>
2022-06-08 20:25:17 +0530

    Sticky Navbar
    Signed-off-by: Graymonkeycap <akhilchakkungal@gmail.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,8 +14,20 @@ const Navbar=()=>{
         const download= await getDownloadURL(ref(storage, 'dummy.pdf'));
         seturl(download);
       }
+    
+      const [colorChange, setColorchange] = useState(false);
+      const changeNavbarColor = () =>{
+         if(window.scrollY >= 300){
+           setColorchange(true);
+         }
+         else{
+           setColorchange(false);
+         }
+      };
+      window.addEventListener('scroll', changeNavbarColor);
+
     return(
-        <div className='navbar'>
+        <div className='navbar' style={colorChange?{background:'black'}:null}>
             <NavLink exact to="/" activeClassName='active'>Home</NavLink>
             <NavLink exact to="/about" >About</NavLink>
             <NavLink exact to="/projects" >Portfolio</NavLink>
